bcc51279dc0bbdbe31180a92d1fa2751d0101445
panwaraporn <30980249+panwaraporn@users.noreply.github.com>
2019-04-08 17:01:48 +0700

    commit1
    push

---
+++ Assets/Script/Box.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    private PlayerController player;
+
+   
+
+    void Start()
+    {
+        player = gameObject.GetComponentInParent<PlayerController>();
+
+
+    }
+
+    void Update()
+    {
+        player.boxx = true;    
+        
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 6;
+    public float speed = 50f;
+    public float jumpPower = 600f;
+
+    public bool grounded;
+    public bool boxx;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+        anim.SetBool("Grounded", grounded);
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+        if (Input.GetAxis("Horizontal") < -0.1f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(Vector2.up * jumpPower);
+            grounded = false;
+        
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rb2d.AddForce((Vector2.right * speed) * h);
+
+        if (rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+
+        if (rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+    }
+
+
+    public void Damage(int dmg)
+    {
+
+    }
+}
